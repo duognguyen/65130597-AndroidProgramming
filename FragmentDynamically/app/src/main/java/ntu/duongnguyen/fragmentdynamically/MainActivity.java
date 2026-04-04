5f9ba6760f13f4c03eea06e68bb2c63eb104ff93
Duong Nguyen <nguyenthaiduong12112005@gmail.com>
2026-04-04 16:53:33 +0700

    BaiTH12: Fragment động

---
+++ FragmentDynamically/app/src/main/java/ntu/duongnguyen/fragmentdynamically/MainActivity.java
@@ -0,0 +1,25 @@
+package ntu.duongnguyen.fragmentdynamically;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.frameContent, new ContentFragment())
+                .add(R.id.frameFooter, new FooterFragment())
+                .commit();
+    }
+}
